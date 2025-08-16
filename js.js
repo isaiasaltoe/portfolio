3dc1da366f3bca666de68142908f904874fc6c9b
isaias <isaiasaltoe7@gmail.com>
2025-08-15 23:53:52 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,12 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.efeito-txt', {
+    duration: 1000,
+    distance: '90px',
+    delay: 100
+})
+
+revelar.reveal('.efeito-titulo', {
+    duration: 500,
+    distance: '90px',
+})
