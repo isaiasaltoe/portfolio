6bcb98fcbae596c53f12e6d74b4e2ec6bd1272bb
isaiasaltoe <isaiasaltoe7@gmail.com>
2026-04-08 00:04:15 -0300

    a

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,9 @@ const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/portfolio',
   assetPrefix: '/portfolio/',
-  images:{
+  images: {
     unoptimized: true,
-  }
+  },
 };
-
-module.exports = nextConfig
 
 export default nextConfig;
