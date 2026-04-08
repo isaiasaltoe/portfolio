83168f50218f28c81c0d37483dffca21ccf78b0f
isaiasaltoe <isaiasaltoe7@gmail.com>
2026-04-07 23:50:08 -0300

    fix: nextjs deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/nome-do-repo',
+  images:{
+    unoptimized: true,
+  }
 };
+
+module.exports = nextConfig
 
 export default nextConfig;
