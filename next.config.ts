41eaec09412ede53d6b62e64764672a15f0bbd21
isaiasaltoe <isaiasaltoe7@gmail.com>
2026-05-07 22:33:25 -0300

    chore:docker settings for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/portfolio' : '', // nome exato do seu repositório no GitHub
   images: {
     unoptimized: true,
   },
